46f324480273ebd5949b60a0f0c8f6a0043eefdc
Abraham "El Pupas" Hernando <ahernandob@iesrafaelalberti.es>
2025-03-11 12:53:00 +0100

    Funcionalidad resta

--- Calc.kt
+++ Calc.kt
@@ -1,12 +1,12 @@
 fun main(args: Array) {
-       val (operation, num1, num2) = parseArguments(args)
-       val result = when (operation) {
-           "multiplica" -> multiplica(num1, num2)
-           "divide" -> divide(num1, num2)
-           else -> throw IllegalArgumentException("Operación no soportada")
-       }
-       showResult(operation, result)
-   }
+    val (operation, num1, num2) = parseArguments(args)
+    val result = when (operation) {
+        "multiplica" -> multiplica(num1, num2)
+        "resta" -> resta(num1, num2)
+        else -> throw IllegalArgumentException("Operación no soportada")
+    }
+    showResult(operation, result)
+}
 
 fun parseArguments(args: Array): Triple {
     if (args.size != 3) {
@@ -16,6 +16,10 @@ fun parseArguments(args: Array): Triple {
     val num1 = args[1].toInt()
     val num2 = args[2].toInt()
     return Triple(operation, num1, num2)
+}
+
+fun resta(num1: Int, num2: Int): Int {
+    return num1 - num2
 }
 
 fun multiplica(num1: Int, num2: Int): Int {
